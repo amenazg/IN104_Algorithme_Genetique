377b80527c206af45d113b0429c4ff3fdd9b96a9
amenazg <114263336+amenazg@users.noreply.github.com>
2023-05-16 17:09:54 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -4,15 +4,15 @@
 #include <stdbool.h>
 
 Individu * getNewIndividu(int l, int *t){
-    // generates a new individual by random
-    Individu * indv  = malloc(sizeof(Individu)); // initialize variable
+    // création d'un nouveau individu 
+    Individu * indv  = malloc(sizeof(Individu)); // initialisation des attributs 
     indv->value = 0;
     indv->next= NULL;
     indv->nb = 0;
     indv->DNA = malloc(l*sizeof(int));
     int cnt=0;
     for(int i=0;i<l;i++){
-        int ra = rand()%60; // choice of DNA with 1/3 probability for every type
+        int ra = rand()%60; // choix de la gene avec une 1/3 probabilté entre {0,1,2}
         if(ra<20){//20
             indv->DNA[i]=0;
             indv->nb++;
@@ -23,11 +23,11 @@ Individu * getNewIndividu(int l, int *t){
             indv->nb++;
             indv->DNA[i]=2;
         }
-        indv->value+=(indv->DNA[i]-1)*t[i];// sum of all elements corresponding to the current configuration
+        indv->value+=(indv->DNA[i]-1)*t[i];// sommation suivant la configuration de l'adn
     }
-    indv->value = abs(indv->value); //  take the absolute value of the sum
+    indv->value = abs(indv->value); // valeur absolue de la somme 
     if(cnt==l){
-        // if the current individual has no elements in his set , then we free the memory and we try again
+        // si l'individu génré ne prend aucun element de l'ensemble de départ dans la somme , on l'elimine 
         free(indv);
         return getNewIndividu(l, t);
     }
@@ -35,7 +35,7 @@ Individu * getNewIndividu(int l, int *t){
 }
 
 Individu * getCopy(Individu * indiv, int l){
-    // to copy the individual without it's next element in the list
+    // copie de l'individu sans avoir un succeur . 
     Individu * indv  = malloc(sizeof(Individu));
     indv->value = indiv->value;
     indv->next= NULL;
@@ -48,49 +48,49 @@ Individu * getCopy(Individu * indiv, int l){
 }
 
 Individu * getElementByIndex (Individu * gen , int idx){
-    // to return the pointer corresponding the element in the idx'th position
-    //printf("idx = %d\n",idx);
-    if(idx == 0)return gen; //  if idx = 0 then we return the current element
-    return getElementByIndex(gen->next, --idx); //  otherwise we decrease the index by 1 and apply the same function to the next element
+    // retoune de l'individu suivant sa position (ou indice ) dans la liste chainée
+    if(idx == 0)return gen; //  si idx=0 , elle retourne l'elemnt actuelle qui correspond à la tête de la liste
+    return getElementByIndex(gen->next, --idx); //  sinon , on décrémentant l'indice et appliquer la fonction au élement suivant  
 }
 
 Individu * GetBestElements(Individu * gen, int k){
-    // to return best k elements in the linked list , but our linked list is always sorted thanks the the inser function , we just need to return the first k element
+    //retourne les meilleures k individus , mais notre liste est triée grace à la fonction insertion .  
     if(gen==NULL)return NULL;
     if(k>0){
         gen->next = GetBestElements(gen->next, k-1);
         return gen;
     }else{
-        gen->next =  GetBestElements(gen->next, k-1); // we apply the function here just to free the memory of the rest of the linked list
+        gen->next =  GetBestElements(gen->next, k-1); // on libere le reste de la liste 
         free(gen);
         return NULL;
     }
 }
 
 Individu * inser(Individu * gen , Individu * indiv, int l){
-    // insert the indv element in the gen in his correct position ==> sorted linked list
+    // insérer l'individus dans la liste chaînée suivant sa pertinence , pour avoir une liste chaînée triée . 
     if(gen==NULL){
         return indiv;
     }
-    if(cmp(indiv, gen)){ //  cmp : it's a function to compare two individuals cmp(a,b) is true iff a is better than b
+    if(cmp(indiv, gen)){ //  cmp : fonction de comparaison entre deux individus .
         indiv->next = gen;
         return indiv;
     }
-    Individu * pcrawl = gen; // pointer to go through the linked list
-    Individu * last = gen; //  last visited element in the linked list
+    Individu * pcrawl = gen; // pointeur pour parcourir la liste
+    Individu * last = gen; //  dérnier individu visité dans la liste 
     while(1){
         if(pcrawl == NULL){
-            // if we reached the end of the linked list we just append it to the last element
+            // si on atteind la fin de la liste on ajoute l'individu à la fin de la liste
             last->next = indiv;
             return gen;
         }else{
             if(!cmp(indiv, pcrawl)){
                 if(eq(indiv, pcrawl,l))return gen;
-                // if the element in pcrawl is better then our new indiv we just keep going
+                // si l'individu dans  pcrawl est plus pertinent  que notre individu , on continue notre parcours .
+                
                 last = pcrawl;
                 pcrawl = pcrawl->next;
             }else{
-                // otherwise we append it before pcrawl that mean to the element before pcrawl which is "last"
+                // sinon on l'insére avant pcrawl cad àprés l'individu last . 
                 last->next = indiv;
                 indiv->next = pcrawl;
                 return gen;
@@ -101,10 +101,10 @@ Individu * inser(Individu * gen , Individu * indiv, int l){
 }
 
 Individu * getInitialGeneration( int n, int* t, int l){
-    // to generate the initial generation with n individuals
-    Individu * gen = getNewIndividu(l,t); //  get the first individual
+    // cette fonction génere la generation initiale de n individus . 
+    Individu * gen = getNewIndividu(l,t); // 1ér individu avant le tri 
     for(int i=0;i<n-1;i++){
-        // append more n-1 individuals
+        // on ajoute les n-1 élements dans l'ordre en fonction de la pertinence . 
         gen = inser(gen,getNewIndividu(l,t),l);
     }
     return gen;
@@ -113,9 +113,7 @@ Individu * getInitialGeneration( int n, int* t, int l){
 
 
 Individu * addChildGen(Individu * gen , int n , int l, int *t){
-    // in this function we will generate the children generation from the best (n/2) individuals randomly
-    // then we will merge the child generation and parent generation
-    // ==> hybrid generation
+    // cette fonction génere les individus de la géneration suivante à partir des meilleures individus e la géneration précedente . 
     Individu  * childGen = NULL;
     double a[n+1];
     double s = 1.7;
@@ -123,28 +121,22 @@ Individu * addChildGen(Individu * gen , int n , int l, int *t){
     for(int i=0;i<n;i++){
         a[i]=(2.0-s)/(double)(n*1.0) + (2.0*(n-i-1)*(s-1))/(double)(1.0*n*(n-1));
         if(i>0)a[i]+=a[i-1];
-    }
+    }// le tableau a contient une distribution de probabilité qui favorise les meilleures individus pour participer à la réproduction 
     double r = ((double)rand() / (double)RAND_MAX ) /((double)1.0*lambda);
-    //printf(" r = %.10lf\n",r);
-    int j=0;
+=    int j=0;
     int current_member=0;
     while(current_member<lambda){
-        while(r<=a[j] && current_member<lambda){
-            int father_idx =current_member ;// choose the index of the father randomly
+        while(r<=a[j] && current_member<lambda){ 
+            int father_idx =current_member ;// choix aléatoire de l'indice du pére 
             int mother_idx = j;
-            /*do{
-                // choose the index of the mother randomly different from the father's one
-                mother_idx = rand()%(n/2);
-            }while(mother_idx==father_idx);*/
-            //printf("%d\n",j);
-            //printf("  %.10lf\n %d ",r,j);
-            Individu * father = getCopy(getElementByIndex(gen,father_idx),l); //  get a copy from the father
-            Individu * mother = getCopy(getElementByIndex(gen,mother_idx),l); //  get a copy from the mother
+            
+            Individu * father = getCopy(getElementByIndex(gen,father_idx),l); 
+            Individu * mother = getCopy(getElementByIndex(gen,mother_idx),l); //  copie des parents 
 
-            croisement(father, mother, t , l); // apply crossover
+            croisement(father, mother, t , l); // on fait le croisement entre les deux parents 
 
-            if(checkIndiv(father,l))childGen = inser(childGen, father,l); // if it's valid append it the child generation
-            if(checkIndiv(mother,l))childGen = inser(childGen, mother,l); // if it's valid append it the child generation
+            if(checkIndiv(father,l))childGen = inser(childGen, father,l); // si l'individu est valide , on l'ajoute à la nouvelle géneration
+            if(checkIndiv(mother,l))childGen = inser(childGen, mother,l); // si l'individu est valide , on l'ajoute à la nouvelle géneration
 
             r = r + (1.0/(double)lambda);
             current_member++;
@@ -153,27 +145,26 @@ Individu * addChildGen(Individu * gen , int n , int l, int *t){
     }
     for(int i=0;i<n;i++){
         int ra =  rand()%1000;
-       // printf("ra = %d\n",ra);
-       // probability 8% to do mutation otherwise crossover
+       // appliquer la mutation suivant une probabilité de 8%
         if(ra<=80){
-            int idx = rand()%(n/2); //  choose index randomly from the best (n/2) elements
-            Individu * to_mutate = getCopy(getElementByIndex(gen,idx),l); // get a copy of the individual
-            mutation(to_mutate, l); // apply mutation to the individual
-            to_mutate->value = fitness(to_mutate, l , t); //  update fitness value
-            to_mutate->nb = fit_nb(to_mutate, l); //  update number of elements taken from the set
-            if(checkIndiv(to_mutate,l))childGen = inser(childGen, to_mutate,l); //  if this individual is valid then we take it
-                                                                    // and append it to the child generation
+            int idx = rand()%(n/2); //  cchoix aléatoire de l'indice de meilleures (n/2) individus . 
+            Individu * to_mutate = getCopy(getElementByIndex(gen,idx),l); 
+            mutation(to_mutate, l); // appliquer la mutation
+            to_mutate->value = fitness(to_mutate, l , t); //  mise à jour de la valeur de fitness
+            to_mutate->nb = fit_nb(to_mutate, l); //mise à jour du nombre des élements pris de l'ensemble de départ .
+            if(checkIndiv(to_mutate,l))childGen = inser(childGen, to_mutate,l); //  si l'individus est valide on l'insére à la liste.
         }else{
             childGen = inser(childGen, getNewIndividu(l,t),l);
         }
     }
 
-    while(childGen){
-        Individu * indiv = getCopy(childGen,l); //  get a copy of the first element
-        Individu * lastindiv = childGen; //  store the first element
-        childGen = childGen->next; // we move by one position
-        free(lastindiv); // destroy the first element
-        gen = inser(gen , indiv,l); // add the child to the generation
+    while(childGen){// cette boucle while a pour but de mettre à jour la liste de generation precedente avec la nouvelle géneration 
+        // et la libération de la liste utilisée dans la fonction
+        Individu * indiv = getCopy(childGen,l); 
+        Individu * lastindiv = childGen; 
+        childGen = childGen->next; 
+        free(lastindiv); 
+        gen = inser(gen , indiv,l);
     }
     return gen;
 }
